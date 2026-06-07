bf930ed0286b7c48e6cb6fa5d0f57ca9461f745a
Latrell233 <1371312849@qq.com>
2026-06-07 23:18:27 +0800

    fix: use BASE_URL for avatar paths

--- src/data/participants.ts
+++ src/data/participants.ts
@@ -1,54 +1,56 @@
 import type { Participant } from '../types'
 
+const BASE = import.meta.env.BASE_URL
+
 export const participants: Participant[] = [
   {
     uid: "Latrell",
     nickname: "Latrell",
     height: 188,
     initialWeight: 107,
-    avatar: "/avatars/Latrell.jpg",
+    avatar: `${BASE}avatars/Latrell.jpg`,
   },
   {
     uid: "Bard",
     nickname: "Bard",
     height: 175,
     initialWeight: 82,
-    avatar: "/avatars/Bard.jpg",
+    avatar: `${BASE}avatars/Bard.jpg`,
   },
   {
     uid: "猪事顺利",
     nickname: "猪事顺利",
     height: 175.6,
     initialWeight: 87.25,
-    avatar: "/avatars/猪事顺利.jpg",
+    avatar: `${BASE}avatars/猪事顺利.jpg`,
   },
   {
     uid: "噤.",
     nickname: "噤.",
     height: 175.5,
     initialWeight: 81,
-    avatar: "/avatars/噤..jpg",
+    avatar: `${BASE}avatars/噤..jpg`,
   },
   {
     uid: "起个名字",
     nickname: "起个名字",
     height: 180,
     initialWeight: 63,
-    avatar: "/avatars/起个名字.jpg",
+    avatar: `${BASE}avatars/起个名字.jpg`,
   },
   {
     uid: "I miss",
     nickname: "I miss",
     height: 173.5,
     initialWeight: 87.5,
-    avatar: "/avatars/I miss.jpg",
+    avatar: `${BASE}avatars/I miss.jpg`,
   },
   {
     uid: "定轴转动的屑刚体",
     nickname: "定轴转动的屑刚体",
     height: 180,
     initialWeight: 91.75,
-    avatar: "/avatars/定轴转动的屑刚体.jpg",
+    avatar: `${BASE}avatars/定轴转动的屑刚体.jpg`,
   },
 ]
 
